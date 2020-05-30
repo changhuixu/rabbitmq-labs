6f692314d83bfe7a1340baa26671b53561c964cd
Unknown <changhuixu@hotmail.com>
2020-05-30 09:22:55 -0500

    Initial commit

---
+++ 04_MultiContainers/OrderApi/RabbitMQ/RabbitMQClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace OrderApi.RabbitMQ
+{
+    public interface IRabbitMQClient
+    {
+        void Publish(string exchange, string routingKey, string payload);
+        void CloseConnection();
+    }
+
+    public class RabbitMQClient : IRabbitMQClient
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public RabbitMQClient(IConnection connection)
+        {
+            _connection = connection;
+            _channel = _connection.CreateModel();
+            _channel.ConfirmSelect();
+        }
+
+        public void Publish(string exchange, string routingKey, string payload)
+        {
+            var props = _channel.CreateBasicProperties();
+            props.AppId = "OrderApi";
+            props.Persistent = true;
+            props.UserId = "ops0";
+            props.MessageId = Guid.NewGuid().ToString("N");
+            props.Timestamp = new AmqpTimestamp(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds());
+            var body = Encoding.UTF8.GetBytes(payload);
+            _channel.BasicPublish(exchange, routingKey, props, body);
+            _channel.WaitForConfirmsOrDie(new TimeSpan(0, 0, 5));
+        }
+
+        public void CloseConnection()
+        {
+            _connection?.Close();
+        }
+    }
+}
---
+++ 04_MultiContainers/README.md
@@ -0,0 +1,23 @@
+# RabbitMQ on Docker Starter: Multi-Containers
+
+```Docker
+docker-compose build
+docker-compose up
+
+# Start RabbitMQ only
+docker-compose run --service-ports rabbitmq
+
+```
+
+## Demo
+
+A basic pub/sub model with a message producer (a Web API app) and a consumer (a Worker Service app).
+
+```bash
+# RabbitMQ management website logins
+admin:admin123 (admin account)
+ops0:ops0 (msg producer account)
+ops1:ops1 (msg consumer account)
+```
+
+![rabbitmq starter](./rabbitmq-starter.gif)
